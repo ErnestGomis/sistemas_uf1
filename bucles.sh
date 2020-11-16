9fd8297698c707bb9ca4f08a8a0d3c4d66cb5ded
ErnestGomis <gxmis791@gmail.com>
2020-11-16 11:17:52 +0100

    Ejercicio bucles

---
+++ bucles.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+echo "(Ejercicio de bucles)"
+
+for VARIABLE in condiciones2.sh; do
+	if [ -e $VARIABLE ]; then
+		echo "Existe: $VARIABLE"
+	else
+		echo "NO existe: $VARIABLE"
+	fi
+done
+
+#AQUÍ ABAJO
+
+
