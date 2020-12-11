d1b5b86bca8b1b17409abeb48775b309e73f6d47
ErnestGomis <gxmis791@gmail.com>
2020-12-11 11:18:46 +0100

    mas comandos de VIM

--- md5mp4.sh
+++ md5mp4.sh
@@ -2,13 +2,23 @@
 
 echo "Ejemplo Bucles y MD5"
 
-for VARIABLE in `ls *.mp4`; do
+EXTENSION="mp3"
+
+for ARCHIVO in `ls *.mp4`; do
 	echo "-------------------"
-	VIDEO=`file $VARIABLE | grep Media`
+	VIDEO=`file $ARCHIVO | grep -i Media`
 	echo $VIDEO
-	if [ $VIDEO != "" ]; then
-		md5sum $VARIABLE
+	if [ "$VIDEO" != "" ]; then
+		#md5sum $ARCHIVO
+		echo "Convirtiendo $ARCHIVO a ..."
+		sleep 4
+		NOMBRE=`echo $ARCHIVO | cut -d "." -f 1`
+
+		ffmpeg -i $ARCHIVO $NOMBRE.$EXTENSION
+		sleep 2
+		clear
 	fi
 done
 
-#AQUÍ ABAJO
+
+
